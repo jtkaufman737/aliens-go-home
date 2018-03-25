6fdce93bb9f75669452474b7ae00ea4b46381fec
Joy <jtkaufman737@gmail.com>
2018-03-24 21:13:58 -0400

    Add cannon functions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,17 @@
-const initalState={
-  message: 'easy to integrate React and Redux, right?',
+import {MOVE_OBJECTS} from '../actions';
+import moveObjects from './moveObjects';
+
+const initialState = {
+  angle: 45,
 };
 
-function reducer(state=initalState) {
-  return state;
+function reducer(state = initialState, action){
+  switch (action.type){
+    case MOVE_OBJECTS:
+      return moveObjects(state,action);
+    default:
+      return state;
+  }
 }
 
 export default reducer;
