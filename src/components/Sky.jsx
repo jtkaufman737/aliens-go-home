62d09f5da84874821118ce80ae4aafe411d725b5
Joy <jtkaufman737@gmail.com>
2018-03-24 20:13:42 -0400

    Add ground element

--- src/components/Sky.jsx
+++ src/components/Sky.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 const Sky = () => {
   const skyStyle = {
-    fill:'#30abef',
+    fill: '#30abef',
   };
   const skyWidth = 5000;
   const gameHeight = 1200;
-  return(
-    <react
+  return (
+    <rect
       style={skyStyle}
       x={skyWidth / -2}
       y={100 - gameHeight}
       width={skyWidth}
+      height={gameHeight}
     />
   );
 };
--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import Sky from './Sky';
 
 const Canvas = () => {
-  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight,window.innerWidth,window.innerHeight];
-  return(
+  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+  return (
     <svg
       id="blob-battle-canvas"
       preserveAspectRatio="xMaxYMax none"
       viewBox={viewBox}
     >
-     <circle cx={0} cy={0} r={50}/>
+      <Sky />
+      <circle cx={0} cy={0} r={50} />
     </svg>
   );
 };
