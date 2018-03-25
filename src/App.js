569826a7880d94898b268d9c026d7223bb04b5e3
Joy <jtkaufman737@gmail.com>
2018-03-24 21:31:43 -0400

    extending main app render to include browser behavior

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
+import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
 import PropTypes from 'prop-types';
 
 class App extends Component {
-  render() {
-    return (
-      <Canvas/>
-    );
+  componentDidMount() {
+    const self= this;
+    setInterval(() => {
+      self.props.moveObjects(self.canvasMousePosition);
+    },10);
+  }
+
+trackMouse(event) {
+  this.canvasMousePosition = getCanvasPosition(event);
+}
+
+render () {
+  return(
+    <Canvas
+      angle={this.props.angle}
+      trackMouse={event => (this.trackMouse(event))}
+    />
+  );
   }
 }
 
 App.propTypes={
-  message: PropTypes.string.isRequired,
-}
+  angle=PropTypes.number.isRequired,
+  moveObjects: PropTypes.func.isRequired,
+};
 
 export default App;
