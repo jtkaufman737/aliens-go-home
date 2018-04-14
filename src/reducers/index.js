f578fbef8afbae32405d457d5b7a7f90111128b5
Joy <jtkaufman737@gmail.com>
2018-04-14 14:15:12 -0400

    adding socket.io functionality

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
-import { MOVE_OBJECTS, START_GAME } from '../actions';
+import { LEADERBOARD_LOADED, LOGGED_IN, MOVE_OBJECTS, START_GAME } from '../actions';
 import moveObjects from './moveObjects';
 import startGame from './startGame';
 
 const initialGameState = {
+  currentPlayer: null,
+  players: null,
   started: false,
   kills: 0,
   lives: 3,
@@ -17,6 +19,16 @@ const initialState = {
 
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case LEADERBOARD_LOADED:
+      return {
+        ...state,
+        players: action.players,
+      };
+    case LOGGED_IN:
+      return {
+        ...state,
+        currentPlayer: action.player,
+      };
     case MOVE_OBJECTS:
       return moveObjects(state, action);
     case START_GAME:
